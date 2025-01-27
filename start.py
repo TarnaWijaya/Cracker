4e41c67ce9936f18850d901951e63d05c1f90b72
Tarna <158784522+TarnaWijaya@users.noreply.github.com>
2025-01-27 18:08:38 +0700

    update start.py

--- start.py
+++ start.py
@@ -3,14 +3,19 @@ File: start.py
 Author: TarnaWijaya 
 Description: Cracker Beta
 '''
+import pyautogui
+import time
+import webbrowser
 
-import pywhatkit as kit
-
-# Format: 'phone_number', 'message', hour, minute
+# Nomor tujuan dan pesan
 phone_number = '+6285135530429'  # Ganti dengan nomor WhatsApp penerima
 message = 'Halo, ini pesan otomatis dari Python!'
-hour = 15  # Jam (format 24 jam)
-minute = 30  # Menit
 
-# Mengirim pesan ke WhatsApp
-kit.sendwhatmsg(phone_number, message, hour, minute)
+# Buka WhatsApp Web
+webbrowser.open(f'https://web.whatsapp.com/send?phone={phone_number}&text={message}')
+time.sleep(10)  # Tunggu beberapa detik agar WhatsApp Web terbuka dan siap
+
+while True:
+    pyautogui.write(message)  # Ketik pesan
+    pyautogui.press('enter')  # Tekan tombol Enter untuk mengirim pesan
+    time.sleep(1)  # Tunggu 1 detik sebelum mengirim pesan lagi
