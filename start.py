9f31f58cf5e8ae037e060af971da02f40eed8678
Tarna <158784522+TarnaWijaya@users.noreply.github.com>
2025-01-27 18:07:21 +0700

    update start.py

--- start.py
+++ start.py
@@ -4,5 +4,13 @@ Author: TarnaWijaya
 Description: Cracker Beta
 '''
 
-import android
-print ("Halo ")
+import pywhatkit as kit
+
+# Format: 'phone_number', 'message', hour, minute
+phone_number = '+6285135530429'  # Ganti dengan nomor WhatsApp penerima
+message = 'Halo, ini pesan otomatis dari Python!'
+hour = 15  # Jam (format 24 jam)
+minute = 30  # Menit
+
+# Mengirim pesan ke WhatsApp
+kit.sendwhatmsg(phone_number, message, hour, minute)
